a3ba32ab9ab2bbc95cf7afc93f8f2f025b4494c8
Pam Rucinque <pamela.rucinque@gmail.com>
2016-07-18 16:52:10 +1000

    Split the commands in methods in the ruby script

--- about_mac.rb
+++ about_mac.rb
@@ -1,10 +1,30 @@
 #!/usr/bin/env ruby
-def print_about_mac
+def get_value_only(property)
+  property.split(':')[1].delete!("\n").strip()
+end
+
+def processor
   #system('system_profiler SPHardwareDataType | grep \'Processor\'')
-  processor=`system_profiler SPHardwareDataType | grep 'Processor'`
+  processor_name=`system_profiler SPHardwareDataType | grep 'Processor\sName'`
+  processor_speed=`system_profiler SPHardwareDataType | grep 'Processor\sSpeed'`
+
+  get_value_only(processor_name) + ' - ' + get_value_only(processor_speed)
+end
+
+def serial_number
   serial_number=`system_profiler SPHardwareDataType | grep Serial`
+  get_value_only(serial_number)
+end
+
+def memory
   memory=`system_profiler SPHardwareDataType | grep Memory`
-  puts processor
+  get_value_only(memory)
+end
+
+def print_about_mac
+  puts "Serial Number: " + serial_number
+  puts "Processor: " + processor
+  puts "Memory: " + memory
 end
 
 print_about_mac
